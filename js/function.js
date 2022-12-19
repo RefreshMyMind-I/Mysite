d216feacc54c8bce7c6c3d141d14df5acb3c454d
RefreshMyMind-I <refreshmymind0@gmail.com>
2022-12-19 17:34:08 +0530

    newfunctionforreadmesecond

--- js/function.js
+++ js/function.js
@@ -2,7 +2,21 @@ function myFunction() {
     var dots = document.getElementById("dots");
     var moreText = document.getElementById("more");
     var btnText = document.getElementById("mybtn");
-  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+  function myFunction1() {
+    var dots = document.getElementById("dots1");
+    var moreText = document.getElementById("more1");
+    var btnText = document.getElementById("mybtn1");
     if (dots.style.display === "none") {
       dots.style.display = "inline";
       btnText.innerHTML = "Read more";
